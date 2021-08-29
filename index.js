4e9a58f202756ad98f54ca99bf6722adf8ca50aa
deus-oc <anishagrawal.2612@gmail.com>
2021-08-29 22:35:03 +0530

    production dummy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 dotenv.config({path: './config/config.env'});
 
+
 connectDB();
 const search = require('./routes/search');
 const app = express();
@@ -13,6 +14,17 @@ if(process.env.NODE_ENV  === 'development') {
 }
 
 app.use('/search', search);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static('client/build'));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const PORT = process.env.PORT || 5000;
 
